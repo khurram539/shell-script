0bf73a1d5254f6ddc44a5c8b07ef96ae393288f3
Your Name <khurram539@hotmail.com>
2024-09-16 15:25:20 -0400

    Updated Nodes & Zones

--- Create_EKS.sh
+++ Create_EKS.sh
@@ -4,12 +4,12 @@
 CLUSTER_NAME="EKS-Cluster"
 VERSION="1.30"
 REGION="us-east-1"
-NODEGROUP_NAME="worker-nodes"
+NODEGROUP_NAME="Worker"
 NODE_TYPE="t2.medium"
 NODE_VOLUME_SIZE=20
 NODE_VOLUME_TYPE="gp3"
-NODES=3
-ZONES="us-east-1a,us-east-1b,us-east-1c"
+NODES=2
+ZONES="us-east-1a,us-east-1b"
 
 # Run eksctl create cluster command
 eksctl create cluster --name $CLUSTER_NAME --version $VERSION --region $REGION --nodegroup-name $NODEGROUP_NAME --node-type $NODE_TYPE --nodes $NODES --zones $ZONES --node-volume-size $NODE_VOLUME_SIZE --node-volume-type $NODE_VOLUME_TYPE
