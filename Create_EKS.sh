396785fde5eeca5b306e0572e73a8428c5a2fea0
root <root@RAZER16.>
2024-09-29 23:35:01 -0400

    lower instance tye

--- Create_EKS.sh
+++ Create_EKS.sh
@@ -5,11 +5,11 @@ CLUSTER_NAME="EKS-Cluster"
 VERSION="1.30"
 REGION="us-east-1"
 NODEGROUP_NAME="Worker"
-NODE_TYPE="t2.medium"
+NODE_TYPE="t2.small"
 NODE_VOLUME_SIZE=20
 NODE_VOLUME_TYPE="gp3"
 NODES=2
-ZONES="us-east-1a,us-east-1b"
+ZONES="us-east-1b,us-east-1c"
 
 # Run eksctl create cluster command
 eksctl create cluster --name $CLUSTER_NAME --version $VERSION --region $REGION --nodegroup-name $NODEGROUP_NAME --node-type $NODE_TYPE --nodes $NODES --zones $ZONES --node-volume-size $NODE_VOLUME_SIZE --node-volume-type $NODE_VOLUME_TYPE
