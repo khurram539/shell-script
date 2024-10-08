ecdda48616b3c759bab6846fc583fccd8710fb33
Your Name <khurram539@hotmail.com>
2024-10-08 10:47:28 -0400

    Upgrade EKS

---
+++ update_EKS.sh
@@ -0,0 +1,11 @@
+# Download the latest version
+curl -LO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
+
+# Extract the tarball
+tar -xzf eksctl_Linux_amd64.tar.gz
+
+# Move the binary to your PATH
+sudo mv eksctl /usr/local/bin/eksctl
+
+# Verify the installation
+eksctl version
